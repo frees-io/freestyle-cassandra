a9eadef1912f2226c9878cc7bdc91a893d44020f
Fede Fernández <federico.f.b@47deg.com>
2017-06-28 20:38:41 +0200

    Adds statement config model and tests

--- core/src/main/scala/config/implicits/package.scala
+++ core/src/main/scala/config/implicits/package.scala
@@ -20,6 +20,7 @@ package config
 import classy.DecodeError.WrongType
 import classy.{Decoder, Read}
 import classy.config._
+import com.datastax.driver.core.Statement
 import com.typesafe.config.Config
 
 import scala.util.{Failure, Success, Try}
@@ -45,5 +46,36 @@ package object implicits {
 
   def read[A, B](f: A => ConfigDecoder[B])(implicit R: Read[Config, A]): Read[Config, B] =
     Read.instance[Config, B](path => readConfig[A](path).flatMap(f))
+
+  final class ConfigStatementOps(cs: ConfigStatement) {
+
+    def applyConf(st: Statement): Statement = {
+
+      import scala.collection.JavaConverters._
+
+      cs.tracingEnabled foreach {
+        case true  => st.enableTracing()
+        case false => st.disableTracing()
+      }
+      cs.consistencyLevel foreach st.setConsistencyLevel
+      cs.serialConsistencyLevel foreach st.setSerialConsistencyLevel
+      cs.defaultTimestamp foreach st.setDefaultTimestamp
+      cs.fetchSize foreach st.setFetchSize
+      cs.idempotent foreach st.setIdempotent
+      cs.outgoingPayload foreach (m => st.setOutgoingPayload(m.asJava))
+      cs.pagingState foreach {
+        case CodecPagingState(ps, Some(cr)) => st.setPagingState(ps, cr)
+        case CodecPagingState(ps, None)     => st.setPagingState(ps)
+        case RawPagingState(array)          => st.setPagingStateUnsafe(array)
+      }
+      cs.readTimeoutMillis foreach st.setReadTimeoutMillis
+      cs.retryPolicy foreach st.setRetryPolicy
+      st
+    }
+
+  }
+
+  implicit def configStatementOps(cs: ConfigStatement): ConfigStatementOps =
+    new ConfigStatementOps(cs)
 
 }
