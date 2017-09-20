32f2c06e495fc3247d4ce1023382aa791566bae9
Fede Fernández <federico.f.b@47deg.com>
2017-09-20 08:38:28 +0200

    Modifies tests

--- core/src/test/scala/mapper/ByteBufferMapperSpec.scala
+++ core/src/test/scala/mapper/ByteBufferMapperSpec.scala
@@ -19,6 +19,7 @@ package mapper
 
 import java.nio.ByteBuffer
 
+import cats.MonadError
 import com.datastax.driver.core.{ProtocolVersion, TypeCodec}
 import org.scalacheck.Prop._
 import org.scalatest.{Matchers, WordSpec}
@@ -46,15 +47,16 @@ class ByteBufferMapperSpec extends WordSpec with Matchers with Checkers {
     "map the fields for a regular case class" in {
 
       import FieldListMapper._
+      import cats.instances.try_._
 
       check {
         forAll { a: A =>
-          val mapper = ByteBufferMapper[A]
-          mapper.map(a).toMap == Map(
-            "a1" -> intCodec.serialize(a.a1),
-            "a2" -> stringCodec.serialize(a.a2),
-            "a3" -> booleanCodec.serialize(a.a3)
-          )
+          val mapper: ByteBufferMapper[A]   = ByteBufferMapper[A]
+          val mapperList: List[FieldMapper] = mapper.map(a)
+          mapperList.size == 3 &&
+          mapperList.head.serialize == intCodec.serialize(a.a1) &&
+          mapperList(1).serialize == stringCodec.serialize(a.a2) &&
+          mapperList(2).serialize == booleanCodec.serialize(a.a3)
         }
       }
 
@@ -68,23 +70,27 @@ class ByteBufferMapperSpec extends WordSpec with Matchers with Checkers {
 
         val Regex: Regex = "(\\d+);(.+)".r
 
-        override def deserialize(bytes: ByteBuffer): B =
-          stringCodec.deserialize(bytes) match {
-            case Regex(v1, v2) => B(v1.toLong, v2)
-            case _             => throw new RuntimeException("Bad serialized value")
+        override def deserialize[M[_]](bytes: ByteBuffer)(
+            implicit M: MonadError[M, Throwable]): M[B] =
+          M.flatMap(stringCodec.deserialize(bytes)) {
+            case Regex(v1, v2) => M.pure(B(v1.toLong, v2))
+            case _             => M.raiseError[B](new RuntimeException("Bad serialized value"))
           }
 
-        override def serialize(value: B): ByteBuffer =
+        override def serialize[M[_]](value: B)(
+            implicit M: MonadError[M, Throwable]): M[ByteBuffer] =
           stringCodec.serialize(value.b1 + ";" + value.b2)
       }
 
+      import cats.instances.try_._
+
       check {
         forAll { c: C =>
-          val mapper = ByteBufferMapper[C]
-          mapper.map(c).toMap == Map(
-            "c1" -> stringCodec.serialize(c.c1),
-            "c2" -> decoder.serialize(c.c2)
-          )
+          val mapper: ByteBufferMapper[C]   = ByteBufferMapper[C]
+          val mapperList: List[FieldMapper] = mapper.map(c)
+          mapperList.size == 2 &&
+          mapperList.head.serialize == stringCodec.serialize(c.c1) &&
+          mapperList(1).serialize == decoder.serialize(c.c2)
         }
       }
 
@@ -93,15 +99,16 @@ class ByteBufferMapperSpec extends WordSpec with Matchers with Checkers {
     "map the fields for a case class with another embedded case class" in {
 
       import FieldMapperExpanded._
+      import cats.instances.try_._
 
       check {
         forAll { c: C =>
-          val mapper = ByteBufferMapper[C]
-          mapper.map(c).toMap == Map(
-            "c1" -> stringCodec.serialize(c.c1),
-            "b1" -> longCodec.serialize(c.c2.b1),
-            "b2" -> stringCodec.serialize(c.c2.b2)
-          )
+          val mapper                        = ByteBufferMapper[C]
+          val mapperList: List[FieldMapper] = mapper.map(c)
+          mapperList.size == 3 &&
+          mapperList.head.serialize == stringCodec.serialize(c.c1) &&
+          mapperList(1).serialize == longCodec.serialize(c.c2.b1) &&
+          mapperList(2).serialize == stringCodec.serialize(c.c2.b2)
         }
       }
 
