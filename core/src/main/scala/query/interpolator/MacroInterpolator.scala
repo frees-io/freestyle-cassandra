9a1a3efb5952dd31370fbbbf62c9dd93e6071505
Fede Fernández <federico.f.b@47deg.com>
2017-10-09 09:02:55 +0200

    Extract some macro definitions from main method for reusing them

--- core/src/main/scala/query/interpolator/MacroInterpolator.scala
+++ core/src/main/scala/query/interpolator/MacroInterpolator.scala
@@ -22,6 +22,51 @@ import scala.meta._
 
 object MacroInterpolator {
 
+  def imports =
+    q"""
+        import cats.MonadError
+        import contextual.{Case, Prefix}
+        import freestyle.cassandra.codecs.ByteBufferCodec
+        import freestyle.cassandra.query.interpolator.{CQLInterpolator, CQLLiteral}
+        import freestyle.cassandra.query.model.SerializableValue
+        import freestyle.cassandra.schema.provider.{SchemaDefinitionProvider, TroySchemaProvider}
+        import freestyle.cassandra.schema.validator.{SchemaValidator, TroySchemaValidator}
+        import java.nio.ByteBuffer
+        import scala.util.Try
+     """
+
+  def schemaFileValidator(typeName: Type.Name, schemaPath: String) =
+    q"""
+        val tryMonadError: MonadError[Try, Throwable] = cats.instances.try_.catsStdInstancesForTry
+        val schemaProvider: SchemaDefinitionProvider[Try] =
+          TroySchemaProvider[Try](${Term.Name(typeName.value)}.getClass.getResourceAsStream(${Lit.String(schemaPath)}))(tryMonadError)
+        val schemaValidator: SchemaValidator[Try] = TroySchemaValidator.instance(tryMonadError, schemaProvider)
+     """
+
+  def companion(typeName: Type.Name) =
+    q"""
+        object ${Term.Name(typeName.value)} {
+
+          object cqlInterpolator extends CQLInterpolator(schemaValidator)
+
+          implicit def embedArgsNamesInCql[T](implicit C: ByteBufferCodec[T]) = cqlInterpolator.embed[T](
+            Case(CQLLiteral, CQLLiteral) { v =>
+              new SerializableValue {
+                override def serialize[M[_]](implicit E: MonadError[M, Throwable]): M[ByteBuffer] =
+                  C.serialize(v)
+              }
+            }
+          )
+
+          final class CQLStringContext(sc: StringContext) {
+            val cql = Prefix(cqlInterpolator, sc)
+          }
+
+          implicit def cqlStringContext(sc: StringContext): CQLStringContext =
+            new CQLStringContext(sc)
+        }
+     """
+
   class SchemaFileInterpolator(schemaPath: String) extends scala.annotation.StaticAnnotation {
 
     inline def apply(defn: Any): Any = meta {
@@ -34,45 +79,7 @@ object MacroInterpolator {
 
       defn match {
         case t: Defn.Trait =>
-          val companion =
-            q"""
-               object ${Term.Name(t.name.value)} {
-                 import cats.MonadError
-                 import contextual.{Case, Prefix}
-                 import freestyle.cassandra.codecs.ByteBufferCodec
-                 import freestyle.cassandra.query.interpolator.{CQLInterpolator, CQLLiteral}
-                 import freestyle.cassandra.query.model.SerializableValue
-                 import freestyle.cassandra.schema.provider.{SchemaDefinitionProvider, TroySchemaProvider}
-                 import freestyle.cassandra.schema.validator.{SchemaValidator, TroySchemaValidator}
-                 import java.nio.ByteBuffer
-                 import scala.util.Try
-
-                 val tryMonadError: MonadError[Try, Throwable] = cats.instances.try_.catsStdInstancesForTry
-
-                 val schemaProvider: SchemaDefinitionProvider[Try] =
-                   TroySchemaProvider[Try](${Term.Name(t.name.value)}.getClass.getResourceAsStream(${Lit.String(arg)}))(tryMonadError)
-                 val schemaValidator: SchemaValidator[Try] = TroySchemaValidator.instance(tryMonadError, schemaProvider)
-
-                 object cqlInterpolator extends CQLInterpolator(schemaValidator)
-
-                 implicit def embedArgsNamesInCql[T](implicit C: ByteBufferCodec[T]) = cqlInterpolator.embed[T](
-                   Case(CQLLiteral, CQLLiteral) { v =>
-                     new SerializableValue {
-                       override def serialize[M[_]](implicit E: MonadError[M, Throwable]): M[ByteBuffer] =
-                         C.serialize(v)
-                     }
-                   }
-                 )
-
-                 final class CQLStringContext(sc: StringContext) {
-                   val cql = Prefix(cqlInterpolator, sc)
-                 }
-
-                 implicit def cqlStringContext(sc: StringContext): CQLStringContext =
-                   new CQLStringContext(sc)
-               }
-           """
-          Term.Block(Seq(t, companion))
+          Term.Block(Seq(t, imports, schemaFileValidator(t.name, arg), companion(t.name)))
         case _ =>
           println(defn.structure)
           abort("@SchemaInterpolator must annotate a trait.")
